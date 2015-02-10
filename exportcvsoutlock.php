dd1ad68f1d030b06befd7e3ef37f792759ca360f
Petr Blaha <petrbla@gmail.com>
2015-02-10 21:44:58 +0100

    Update exportcvsoutlock.php

--- exportcvsoutlock.php
+++ exportcvsoutlock.php
@@ -7,14 +7,16 @@
  */
  
 include_once './conectdb.php';
- 
-function array2csv(array &$array){
-   if(count($array)==0){return null;}
-   ob_start();
-   $df = fopen("php://output", 'w');
-   foreach($array as $row){fputcsv($df, $row);}
-   fclose($df);
-   return ob_get_clean();
+
+
+function sputcsv($row, $delimiter = ',', $enclosure = '"', $eol = "\n"){
+    static $fp = false;
+    if ($fp === false){$fp = fopen('php://temp', 'r+');}else{rewind($fp);}
+    if (fputcsv($fp, $row, $delimiter, $enclosure) === false){return false;}
+    rewind($fp);
+    $csv = fgets($fp);
+    if ($eol != PHP_EOL){$csv = substr($csv, 0, (0 - strlen(PHP_EOL))) . $eol;}
+    return $csv;
 }
 
 function download_send_headers($filename) {
@@ -34,7 +36,7 @@ function download_send_headers($filename) {
 $jmeno=iconv('utf-8','windows-1250', "Jméno");
 $email=iconv('utf-8','windows-1250', "E-mailová adresa");
 $list[]=array("$jmeno", "$email");
-$vysledek = mysql_query("SELECT * FROM `user` ORDER BY `id` ASC");
+$vysledek = mysql_query("SELECT * FROM `uzivatele` ORDER BY `id` ASC");
 while ($zaznam = MySQL_Fetch_Array($vysledek)){
 	$jmeno=iconv('utf-8','windows-1250', $zaznam[jmeno]);
 	$email=iconv('utf-8','windows-1250', $zaznam[email]);
@@ -42,5 +44,6 @@ while ($zaznam = MySQL_Fetch_Array($vysledek)){
 }
 
 download_send_headers("zakaznik_export_" . date("Y-m-d") . ".csv");
-echo array2csv($list);
+if (PHP_EOL == "\r\n"){$eol="\n";}else{$eol = "\r\n";}
+foreach($list as $row){echo sputcsv($row, ',', '"', $eol);}
 die();
